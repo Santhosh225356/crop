021920746f0b2ce883619244a9cc76fb520ff0ee
Santhosh <thalapathysanthosh836@gmail.com>
2025-06-08 17:42:25 +0530

    basic app

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About FarmGuard</h1>
+      <p>FarmGuard is an AI-powered crop disease detection and advisory system designed to assist farmers.</p>
+    </div>
+  );
+}
---
+++ src/pages/contact.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Email: support@farmguard.ai</p>
+      <p>Phone: +91-12345-67890</p>
+    </div>
+  );
+}
